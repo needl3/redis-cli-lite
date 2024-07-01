afd742346405ec853c47c80a7ea1c594fe39e412
Anish Chapagai <57314527+needl3@users.noreply.github.com>
2024-07-01 17:39:30 +0545

    refactor: directory name change

--- pkg/serializer/parser.go
+++ pkg/serializer/parser.go
@@ -6,17 +6,26 @@ package serializer
 import "strings"
 
 type Parser struct {
-	RETURN_TYPE string
+	OK_TYPE      string
+	ERROR_TYPE   string
+	CLRF         string
+	I_ARR        string
+	I_BULKSTRING string
 }
 
 var ParserClient Parser
 
 func init() {
 	ParserClient = Parser{
-		RETURN_TYPE: "+",
+		OK_TYPE:      "+",
+		ERROR_TYPE:   "-",
+		CLRF:         "\r\n",
+		I_ARR:        "*",
+		I_BULKSTRING: "$",
 	}
 }
 
 func (prsr Parser) Parse(resp []byte) string {
+	// Time for some state machine
 	return strings.Trim(string(resp), "+")
 }
